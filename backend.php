1f71d05eb5b756e74e8555377372c485e59033ae
Aksn09 <iakashs660@gmail.com>
2021-11-01 01:36:23 +0530

    new commit

--- backend.php
+++ backend.php
@@ -9,23 +9,9 @@
             $sql = $conn->query($sql);
             $sql = $sql-> fetch_assoc();
             if($sql){
-                echo $sql['name']; 
+                echo "<p>Hey! " . $sql['name']; 
             exit();
+            }
         }
-    }else if(isset($_POST['grosssubmit'])){
-        $rollno = $_POST['roll_no'];
-        $rollno = mysqli_real_escape_string($conn, $rollno);
-        $sql = "Select COUNT(attendance_id) From attendance Where roll_no = '$rollno'";  
-        $count = array_count_values($sql);
-        $sql = $conn->query($sql);
-            $sql = $sql-> fetch_assoc();
-            if($sql){
-                // echo $sql;
-                print_r($count);
-            exit();
-        }
-    }
-
-    
 }
 ?>
