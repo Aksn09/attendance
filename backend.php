f170c95e5cc3c46c4f1c5545193a740c286ff281
Aksn09 <iakashs660@gmail.com>
2021-11-01 18:20:09 +0530

    final

--- backend.php
+++ backend.php
@@ -1,5 +1,6 @@
 <?php
     if($_POST){
+        // require $_SERVER['DOCUMENT_ROOT'] . '/connection.php';
         require 'connection.php';
         $conn = connect_db();
         if(isset($_POST['rollsubmit'])){
@@ -10,6 +11,44 @@
             $sql = $sql-> fetch_assoc();
             if($sql){
                 echo "<p>Hey! " . $sql['name']; 
+            exit();
+            }
+        }else if(isset($_POST['attensubmit'])){
+                $roll = $_POST['roll_no'];
+                $roll = mysqli_real_escape_string($conn, $roll);
+                $sql = "Select date, presence From attendance Where roll_no = '$roll'";  
+                $sql = $conn->query($sql);
+                $sql = $sql-> fetch_assoc();
+                while ($sql) {
+                    echo $sql['date'] . "  ---  " . $sql['presence'];
+                exit();
+                }
+        }else if(isset($_POST['findatten'])){
+            $varroll = $_POST['roll_no'];
+            $vardate = $_POST['datetime'];
+            $sql = "Select * from attendance where date = '$vardate' and roll_no = '$varroll'";
+            $sql = $conn->query($sql);
+            $sql = $sql-> fetch_assoc();
+            while ($sql) {
+                echo $sql['presence'];
+            exit();
+            }
+        }else if(isset($_POST['findstr'])){
+            $vardate1 = $_POST['datetime'];
+            $sql = "Select SUM(presence) from attendance where date = '$vardate1'";
+            $sql = $conn->query($sql);
+            $sql = $sql-> fetch_assoc();
+            while ($sql) {
+                echo $sql['SUM(presence)'];
+            exit();
+            }
+        }else if(isset($_POST['avgatten'])){
+            $varroll2 = $_POST['roll_no'];
+            $sql = "Select AVG(presence) from attendance where roll_no = '$varroll2'";
+            $sql = $conn->query($sql);
+            $sql = $sql-> fetch_assoc();
+            while ($sql) {
+                echo $sql['AVG(presence)'];
             exit();
             }
         }
